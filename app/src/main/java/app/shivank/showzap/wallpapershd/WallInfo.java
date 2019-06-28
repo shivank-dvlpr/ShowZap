3f8893f3cfd064edd22d51789ae703fa74eaaf35
HBot <shivankyadav1234@gmail.com>
2019-06-28 21:48:06 +0530

    WallInfo - Blur Background.

--- app/src/main/java/app/shivank/showzap/wallpapershd/WallInfo.java
+++ app/src/main/java/app/shivank/showzap/wallpapershd/WallInfo.java
@@ -1,15 +1,20 @@
 package app.shivank.showzap.wallpapershd;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,17 +30,25 @@ public class WallInfo extends AppCompatActivity {
     String key;
     String Bundle;
     String Value;
+    ImageView wallInfoBG;
+    Bitmap bundleImage;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_wall_info);
+
+        getSupportActionBar().hide();
 
         txtArtBy = findViewById(R.id.txtArtBy);
         txtSource = findViewById(R.id.txtSource);
         txtArtName = findViewById(R.id.txtArtName);
         txtHead = findViewById(R.id.txtHead);
+        wallInfoBG = (ImageView) findViewById(R.id.wallInfoBG);
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
         databaseReference.keepSynced(true);
@@ -44,6 +57,12 @@ public class WallInfo extends AppCompatActivity {
         key = intent.getStringExtra("Data");
         Bundle = intent.getStringExtra("Bundle");
         Value = intent.getStringExtra("Value");
+
+
+        Glide.with(this)
+                .load(Bundle)
+                .override(150,150)
+                .into(wallInfoBG);
 
         if (Bundle != null && Value != null) {
             if (Bundle.equals(Value)) {
@@ -124,7 +143,6 @@ public class WallInfo extends AppCompatActivity {
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 txtArtNameString = dataSnapshot.getValue(String.class);
-                Log.d("NAMEE", Bundle + " - " + Value);
                 txtArtName.setText(txtArtNameString);
 
 
